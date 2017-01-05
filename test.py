41370b3f07505b470cdda57b779ab03795613ed1
wujian-5 <243097783@qq.com>
2017-01-05 14:31:27 +0800

    doubly list insert function

--- test.py
+++ test.py
@@ -1,211 +1,67 @@
-class Member(object):
-    def __init__(self, founder):
-        """
-        founder: string
-        Initializes a member.
-        Name is the string of name of this node,
-        parent is None, and no children
-        """
-        self.name = founder
-        self.parent = None
-        self.children = []
+class Frob(object):
+    def __init__(self, name):
+        self.name = name
+        self.before = None
+        self.after = None
 
-    def __str__(self):
+    def setBefore(self, before):
+        # example: a.setBefore(b) sets b before a
+        self.before = before
+
+    def setAfter(self, after):
+        # example: a.setAfter(b) sets b after a
+        self.after = after
+
+    def getBefore(self):
+        return self.before
+
+    def getAfter(self):
+        return self.after
+
+    def myName(self):
         return self.name
 
-    def add_parent(self, mother):
-        """
-        mother: Member
-        Sets the parent of this node to the `mother` Member node
-        """
-        self.parent = mother
 
-    def get_parent(self):
-        """
-        Returns the parent Member node of this Member
-        """
-        return self.parent
-
-    def is_parent(self, mother):
-        """
-        mother: Member
-        Returns: Boolean, whether or not `mother` is the
-        parent of this Member
-        """
-        return self.parent == mother
-
-    def add_child(self, child):
-        """
-        child: Member
-        Adds another child Member node to this Member
-        """
-        self.children.append(child)
-
-    def is_child(self, child):
-        """
-        child: Member
-        Returns: Boolean, whether or not `child` is a
-        child of this Member
-        """
-        return child in self.children
+eric = Frob('eric')
+andrew = Frob('andrew')
+ruth = Frob('ruth')
+fred = Frob('fred')
+martha = Frob('martha')
 
 
-class Family(object):
-    def __init__(self, founder):
-        """
-        Initialize with string of name of oldest ancestor
+def insert(atMe, newFrob):
+    """
+    atMe: a Frob that is part of a doubly linked list
+    newFrob: a Frob with no links
+    This procedure appropriately inserts newFrob into the linked list that atMe is a part of.    
+    """
+    while atMe != None and atMe.myName() < newFrob.myName():
+        before = atMe
+        atMe = atMe.getAfter()
+        after = atMe
+    while atMe != None and atMe.myName() > newFrob.myName():
+        after = atMe
+        atMe = atMe.getBefore()
+        before = atMe
 
-        Keyword arguments:
-        founder -- string of name of oldest ancestor
-        """
-
-        self.names_to_nodes = {}
-        self.root = Member(founder)
-        self.names_to_nodes[founder] = self.root
-
-    def set_children(self, mother, list_of_children):
-        """
-        Set all children of the mother.
-
-        Keyword arguments:
-        mother -- mother's name as a string
-        list_of_children -- children names as strings
-        """
-        # convert name to Member node (should check for validity)
-        mom_node = self.names_to_nodes[mother]
-        # add each child
-        for c in list_of_children:
-            # create Member node for a child
-            c_member = Member(c)
-            # remember its name to node mapping
-            self.names_to_nodes[c] = c_member
-            # set child's parent
-            c_member.add_parent(mom_node)
-            # set the parent's child
-            mom_node.add_child(c_member)
-
-    def is_parent(self, mother, kid):
-        """
-        Returns True or False whether mother is parent of kid.
-
-        Keyword arguments:
-        mother -- string of mother's name
-        kid -- string of kid's name
-        """
-        mom_node = self.names_to_nodes[mother]
-        child_node = self.names_to_nodes[kid]
-        return child_node.is_parent(mom_node)
-
-    def is_child(self, kid, mother):
-        """
-        Returns True or False whether kid is child of mother.
-
-        Keyword arguments:
-        kid -- string of kid's name
-        mother -- string of mother's name
-        """
-        mom_node = self.names_to_nodes[mother]
-        child_node = self.names_to_nodes[kid]
-        return mom_node.is_child(child_node)
-
-    def cousin(self, a, b):
-        """
-        Returns a tuple of (the cousin type, degree removed)
-
-        Keyword arguments:
-        a -- string that is the name of node a
-        b -- string that is the name of node b
-
-        cousin type:
-          -1 if a and b are the same node.
-          -1 if either one is a direct descendant of the other
-          >=0 otherwise, it calculates the distance from
-          each node to the common ancestor.  Then cousin type is
-          set to the smaller of the two distances, as described
-          in the exercises above
-
-        degrees removed:
-          >= 0
-          The absolute value of the difference between the
-          distance from each node to their common ancestor.
-        """
-        acopy, bcopy = a, b
-        i = 0
-        j = 0
-        if self.names_to_nodes[a] != self.root:
-            while True:
-                if self.names_to_nodes[a].get_parent() == self.root:
-                    break
-                else:
-                    i += 1
-                    a = str(self.names_to_nodes[a].get_parent())
-        if self.names_to_nodes[b] != self.root:
-            while True:
-                if self.names_to_nodes[b].get_parent() == self.root:
-                    break
-                else:
-                    j += 1
-                    b = str(self.names_to_nodes[b].get_parent())
-        if i > j:
-            r = i - j
-            for n in range(r):
-                acopy = str(self.names_to_nodes[acopy].get_parent())
-            t = 0
-            while acopy != acopy:
-                t += 1
-                acopy = str(self.names_to_nodes[acopy].get_parent())
-                bcopy = str(self.names_to_nodes[bcopy].get_parent())
-        else:
-            r = j - i
-            for n in range(r):
-                bcopy = str(self.names_to_nodes[bcopy].get_parent())
-            t = 0
-            while acopy != bcopy:
-                t += 1
-                acopy = str(self.names_to_nodes[acopy].get_parent())
-                bcopy = str(self.names_to_nodes[bcopy].get_parent())
-        return t, r
+    if before != None:
+        newFrob.setBefore(before)
+        before.setAfter(newFrob)
+    if after != None:
+        newFrob.setAfter(after)
+        after.setBefore(newFrob)
 
 
-f = Family("a")
-f.set_children("a", ["b", "c"])
-f.set_children("b", ["d", "e"])
-f.set_children("c", ["f", "g"])
+insert(eric, andrew)
+insert(eric, ruth)
+insert(eric, fred)
+insert(ruth, martha)
 
-f.set_children("d", ["h", "i"])
-f.set_children("e", ["j", "k"])
-f.set_children("f", ["l", "m"])
-f.set_children("g", ["n", "o", "p", "q"])
-
-words = ["zeroth", "first", "second", "third", "fourth", "fifth", "non"]
-
-# These are your test cases.
-
-# The first test case should print out:
-# 'b' is a zeroth cousin 0 removed from 'c'
-t, r = f.cousin("b", "c")
-print "'b' is a", words[t], "cousin", r, "removed from 'c'"
-
-# For the remaining test cases, use the graph to figure out what should
-# be printed, and make sure that your code prints out the appropriate values.
-
-t, r = f.cousin("d", "f")
-print "'d' is a", words[t], "cousin", r, "removed from 'f'"
-
-t, r = f.cousin("i", "n")
-print "'i' is a", words[t], "cousin", r, "removed from 'n'"
-
-t, r = f.cousin("q", "e")
-print "'q' is a", words[t], "cousin", r, "removed from 'e'"
-
-t, r = f.cousin("h", "c")
-print "'h' is a", words[t], "cousin", r, "removed from 'c'"
-
-t, r = f.cousin("h", "a")
-print "'h' is a", words[t], "cousin", r, "removed from 'a'"
-
-t, r = f.cousin("h", "h")
-print "'h' is a", words[t], "cousin", r, "removed from 'h'"
-
-t, r = f.cousin("a", "a")
-print "'a' is a", words[t], "cousin", r, "removed from 'a'"
+person = andrew
+while person != None:
+    print person.myName()
+    person = person.getAfter()
+person = ruth
+while person != None:
+    print person.myName()
+    person = person.getBefore()
